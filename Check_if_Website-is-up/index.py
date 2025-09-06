66e4e90276f415d4330ab9d74a7ffa5a572b66b8
Manoj C T <115369089+ManojCT764@users.noreply.github.com>
2025-09-06 09:54:08 +0000

    write a script to check if a website is UP or Down

---
+++ Check_if_Website-is-up/index.py
@@ -0,0 +1,53 @@
+import requests
+import smtplib
+from email.mime.text import MIMEText   # Used for sending plain text
+from email.mime.multipart import MIMEMultipart   # Used for sending attachments
+import os  # appart from os module we can als use dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def check_website(url):
+    try:
+        response =  requests.get(url)
+        print(response)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+def send_email(sender_email, password, receiver_email, subject, body):
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['to'] = receiver_email
+    message['subject'] = subject
+    message.attach(MIMEText(body, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls() # Start TLS Encryption
+        server.login(sender_email, password)  # FIXED
+        server.sendmail(sender_email, receiver_email, message.as_string())  # FIXED
+        server.quit() # Close the connection 
+        print("Email Sent succesfuly")
+
+    except:
+        print("Failed to send the email")
+
+
+url = "https://googliyaann.com"
+# print(check_website("https://faxitudegyiuhdjnbis.com"))
+
+if not check_website(url):
+    sender_email = "itestmanoj@gmail.com"
+    subject = "Website is Down"
+    body = "warning your website service PING is WARNING PING WARNING - Packet loss = 20%, RTA = 21.84 ms"
+    receiver_email = os.getenv("RECEIVER_EMAIL")
+    password = os.getenv("APP_PASSWORD")  # USing App password Google
+    send_email(sender_email, password, receiver_email, subject, body)
+else:
+    print("Website is Up")
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Important_bash_AND_python_scripts
 Important bash &amp; python scripts
+
+---------- Notes -----------
+
+Infra scripts
+
+Problem 1:
+
+    We have to write a script to check if a website is UP or Down 
+    and if the webite is down send an email to particular Owner.
+    The script can be written in python or bash. 
+    It should expect a website url to be checked
+    and an email address on which we have to send a notification.
+
+
+Packages used in this Env:
+    Package            Version
+------------------ --------
+certifi            2025.8.3
+charset-normalizer 3.4.3
+idna               3.10
+pip                23.2.1
+requests           2.32.5
+urllib3            2.5.0
